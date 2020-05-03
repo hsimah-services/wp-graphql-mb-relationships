547550ea6c8adb421484a8ce732fe2f0bc887acb
hsimah <hamishblake+github@gmail.com>
2020-05-03 12:42:05 -0700

    refactor resolver selection

--- class-config.php
+++ class-config.php
@@ -1,5 +1,11 @@
 <?php
 
+use GraphQL\Error\InvariantViolation;
+use WPGraphQL\Data\Connection\PostObjectConnectionResolver;
+use WPGraphQL\Data\Connection\TermObjectConnectionResolver;
+use WPGraphQL\Data\Connection\UserConnectionResolver;
+use WPGraphQL\Data\DataSource;
+
 final class WPGraphQL_MB_Relationships_Config
 {
   /**
@@ -23,6 +29,11 @@ final class WPGraphQL_MB_Relationships_Config
   public $connection_name;
 
   /**
+   * GraphQL connection type name
+   */
+  public $connection_type;
+
+  /**
    * GraphQL connection arguments
    */
   public $connection_args;
@@ -37,9 +48,34 @@ final class WPGraphQL_MB_Relationships_Config
    */
   public $resolve_node = null;
 
+  /**
+   * WordPress object type
+   * post | user | term
+   */
+  public $object_type_name;
+
   function __construct($settings)
   {
-    $this->type_name = $settings['field']['post_type'];
+    $this->object_type_name = $settings['object_type'];
+
+    switch ($this->object_type_name) {
+      default:
+      case 'post':
+        $this->type_name = $settings['field']['post_type'];
+        $this->type_object = get_post_type_object($this->type_name);
+        $this->graphql_type_name = $this->type_object->graphql_single_name;
+        break;
+        // case 'term':
+        //   $this->type_name = $settings['field']['taxonomy'];
+        //   $this->type_object = get_taxonomy($this->type_name);
+        //   break;
+      case 'user':
+        $this->type_name = 'user';
+        $this->type_object = null;
+        $this->graphql_type_name = 'User';
+        break;
+    }
+
     $this->connection_name = $settings['graphql_name'];
     $this->connection_args = isset( $settings['graphql_args'] ) ? $settings['graphql_args'] : [];
     $this->type_object = get_post_type_object($this->type_name);
@@ -61,7 +97,53 @@ final class WPGraphQL_MB_Relationships_Config
    */
   public function should_register()
   {
-    return $this->type_object !== null &&
-      $this->type_object->show_in_graphql;
+    return $this->type_name === 'user' ||
+      ($this->type_object !== null &&
+        $this->type_object->show_in_graphql);
+  }
+
+  /**
+   * Get applicable connection resolver
+   *
+   * @access public
+   * @since  0.4.0
+   * @return void
+   */
+  public function get_resolver(...$args)
+  {
+    switch ($this->object_type_name) {
+      case 'post':
+        $resolver = new PostObjectConnectionResolver(...$args);
+        // Meta Box does not want post_parent set
+        $resolver->setQueryArg('post_parent', null);
+        return $resolver;
+        // case 'term':
+        //   return new TermObjectConnectionResolver(...$args);
+      case 'user':
+        return new UserConnectionResolver(...$args);
+    }
+
+    throw new InvariantViolation('Unsupported object_type_name for wpgraphql-mb-relationships');
+  }
+
+  /**
+   * Get applicable node resolver
+   *
+   * @access public
+   * @since  0.4.0
+   * @return void
+   */
+  public function get_node_resolver($node, $context)
+  {
+    switch ($this->object_type_name) {
+      case 'post':
+        return DataSource::resolve_post_object($node->ID, $context);
+        // case 'term':
+        //   return DataSource::resolve_term_object($node->term_id, $context);
+      case 'user':
+        return DataSource::resolve_user($node->userID, $context);
+    }
+
+    throw new InvariantViolation('Unsupported object_type_name for wpgraphql-mb-relationships');
   }
 }
