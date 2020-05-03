b448b3396974b20c353ec2ca5f8011fa03923655
hsimah <hamishblake+github@gmail.com>
2020-05-03 08:22:33 -0700

    unidirectional relationships and custom resolvers

---
+++ class-config.php
@@ -0,0 +1,67 @@
+<?php
+
+final class WPGraphQL_MB_Relationships_Config
+{
+  /**
+   * Type name
+   */
+  public $type_name;
+
+  /**
+   * Type object
+   */
+  public $type_object;
+
+  /**
+   * GraphQL type name
+   */
+  public $graphql_type_name;
+
+  /**
+   * GraphQL connection name
+   */
+  public $connection_name;
+
+  /**
+   * GraphQL connection arguments
+   */
+  public $connection_args;
+
+  /**
+   * GraphQL connection resolve function
+   */
+  public $resolve = null;
+
+  /**
+   * GraphQL connection resolve node function
+   */
+  public $resolve_node = null;
+
+  function __construct($settings)
+  {
+    $this->type_name = $settings['field']['post_type'];
+    $this->connection_name = $settings['graphql_name'];
+    $this->connection_args = $settings['graphql_args'];
+    $this->type_object = get_post_type_object($this->type_name);
+    $this->graphql_type_name = $this->type_object->graphql_single_name;
+    if (array_key_exists('resolve', $settings)) {
+      $this->resolve = $settings['resolve'];
+    }
+    if (array_key_exists('resolveNode', $settings)) {
+      $this->resolve_node = $settings['resolveNode'];
+    }
+  }
+
+  /**
+   * Register WPGraphQL MB Relationships config.
+   *
+   * @access public
+   * @since  0.3.0
+   * @return void
+   */
+  public function should_register()
+  {
+    return $this->type_object !== null &&
+      $this->type_object->show_in_graphql;
+  }
+}
--- wp-graphql-mb-relationships.php
+++ wp-graphql-mb-relationships.php
@@ -6,14 +6,14 @@
  * Description: WP GraphQL provider for MB Relationships
  * Author: hsimah
  * Author URI: http://www.hsimah.com
- * Version: 0.2.0
+ * Version: 0.3.0
  * Text Domain: wpgraphql-mb-relationships
  * License: GPL-3
  * License URI: https://www.gnu.org/licenses/gpl-3.0.html
  *
  * @package  WPGraphQL_MB_Relationships
  * @author   hsimah
- * @version  0.2.0
+ * @version  0.3.0
  */
 
 // Exit if accessed directly.
